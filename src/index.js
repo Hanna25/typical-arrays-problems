3ecf97a85d2137a4f38c01c713b14b3afb71911e
Anna <a.sobolenko1194@gmail.com>
2020-02-23 23:47:32 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0 ){
+    return 0
+  } 
+  let minNumber = array[0]
+  for (let i = 1; i < array.length; i++){
+    if (minNumber > array[i]){
+       minNumber = array[i]
+    }     
+  }
+  return minNumber
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0 ){
+    return 0
+  } 
+  let minNumber = array[0]
+  for (let i = 1; i < array.length; i++){
+    if (minNumber < array[i]){
+       minNumber = array[i]
+    }     
+  }
+  return minNumber
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0 ){
+    return 0
+  } 
+  let minNumber = array[0]
+  for (let i = 1; i < array.length; i++){
+   minNumber += array[i]         
+  }
+  return minNumber / array.length
 }
